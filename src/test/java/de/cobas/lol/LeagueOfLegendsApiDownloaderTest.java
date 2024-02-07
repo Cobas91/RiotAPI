2a05b0e29c646047cb6d143616ff5e9e72f2b815
Stephan Gärtner <stephan.gaertner72@outlook.de>
2024-02-07 21:52:27 +0100

    refactor(tests): builder
    - use the builder pattern in tests

--- src/test/java/de/cobas/lol/LeagueOfLegendsApiDownloaderTest.java
+++ src/test/java/de/cobas/lol/LeagueOfLegendsApiDownloaderTest.java
@@ -1,5 +1,9 @@
 package de.cobas.lol;
 
+import de.cobas.lol.client.LeagueOfLegendsClient;
+import de.cobas.lol.client.LeagueOfLegendsClientBuilder;
+import de.cobas.lol.client.VersionDownloader;
+import de.cobas.lol.enums.Language;
 import de.cobas.lol.model.Champion;
 import org.apache.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
@@ -11,29 +15,30 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LeagueOfLegendsApiDownloaderTest {
-    LeagueOfLegendsApiDownloader downloader = new LeagueOfLegendsApiDownloader();
     Logger log = Logger.getLogger(LeagueOfLegendsApiDownloaderTest.class);
 
     @Test
     @DisplayName("Test if the Download of the current Versions numbers return min. 1 Number.")
     public void testVersionDownload(){
-        String[] versions = downloader.downloadVersionNumbers();
+        String[] versions = VersionDownloader.getVersionNumbers();
         assertTrue(versions.length > 0);
     }
     @Test
     @DisplayName("Test if the Download of all Champions is not a Empty result")
     public void testDownloadAllChampions(){
-        List<Champion> allChampions = downloader.getAllChampions();
+        LeagueOfLegendsClient client = new LeagueOfLegendsClientBuilder().withChampionDownloader(Language.en_US).build();
+        List<Champion> allChampions = client.getChampionDownloader().getAllChampions();
          assertFalse(allChampions.isEmpty());
     }
 
     @Test
     @DisplayName("Test if the Download of the icons for one champ is successfully")
     public void testDownloadAllSplashIcons(){
-        List<Champion> allChampions = downloader.getAllChampions();
+        LeagueOfLegendsClient client = new LeagueOfLegendsClientBuilder().withChampionDownloader(Language.en_US).build();
+        List<Champion> allChampions = client.getChampionDownloader().getAllChampions();
         Champion testChamp = allChampions.get(0);
-        downloader.setDefaultChampSplash(testChamp);
-        downloader.setSkinSplashes(testChamp);
+        client.getChampionDownloader().setDefaultChampSplash(testChamp);
+        client.getChampionDownloader().setSkinSplashes(testChamp);
         assertNotNull(testChamp.getChampionIcon());
         assertNotNull(testChamp.getSkins().get(3).getImage());
     }
