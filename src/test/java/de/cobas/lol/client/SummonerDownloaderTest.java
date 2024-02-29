539e94b74944bbc334a3d812c55c7cd0b19e361b
Stephan Gärtner <stephan.gaertner72@outlook.de>
2024-02-29 22:33:44 +0100

    feat(matchdownloader): matchdownload list
    - impl download of match list
    
    Signed-off-by: Stephan Gärtner <stephan.gaertner72@outlook.de>

--- src/test/java/de/cobas/lol/client/SummonerDownloaderTest.java
+++ src/test/java/de/cobas/lol/client/SummonerDownloaderTest.java
@@ -1,16 +1,16 @@
 package de.cobas.lol.client;
 
 import de.cobas.lol.responses.SummonerInformationResponse;
+import lombok.Value;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.PreconditionViolationException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SummonerDownloaderTest {
-
     @Test
     public void testSummonerInformationDownload(){
-        LeagueOfLegendsClient client = new LeagueOfLegendsClientBuilder().withSummonerDownloader("RGAPI-e1d46b42-0845-4538-b780-7c8a66a8ba86").build();//TODO MOCKING
+        LeagueOfLegendsClient client = new LeagueOfLegendsClientBuilder().withSummonerDownloader(System.getProperty("${RIOT_API_TOKEN}")).build();//TODO MOCKING
         SummonerInformationResponse summonerInformation = client.getSummonerDownloader().getSummonerInformation("Cobas");
         assertEquals("Cobas", summonerInformation.getName());
     }
